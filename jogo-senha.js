21afe85dde291cef6a6d378a50104a8660ed1a64
Felipe Teixeira Moraes <felipetm6@hotmail.com>
2018-11-14 21:44:34 -0200

    Adicionar solução do jogo da senha

--- jogo-senha.js
+++ jogo-senha.js
@@ -18,3 +18,40 @@
 - O usuário precisa acertar a senha por completo;
 - O usuário tem 10 tentativas de acerto;
 */
+
+let senha = '';
+
+for (let i = 0; i < 5; i ++) {
+  senha += `${gerarAleatorio(0, 9)}`;
+}
+
+console.log(senha);
+
+for (let i = 5; i >= 0; i --) {
+  const palpite = prompt('Diga qual senha você acredita que foi sorteada. A senha precisa ter 5 digitos!');
+
+  let saida = '';
+
+  for (let j = 0; j < senha.length; j++) {
+    let caractereSenha = senha[j];
+    let caracterePalpite = palpite[j];
+
+    if (senha === palpite) {
+      break;
+    }
+
+    if (caractereSenha === caracterePalpite) {
+      saida += 'O';
+    }
+    else {
+      if (senha.includes(caracterePalpite)) {
+        saida += '-';
+      }
+      else {
+        saida += 'X';
+      }
+    }
+  }
+
+  console.log(saida);
+}
